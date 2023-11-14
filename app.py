11f862be4cafac7448f9adb12cab700964ab5343
nabilabptr <75235781+nabilabptr@users.noreply.github.com>
2023-11-14 12:42:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,45 +8,53 @@ conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 data = conn.read(worksheet="db", usecols=list(range(4)))
 data = data.dropna(how="all")
 
-st.title("Monitoring Rekrutmen Mitra Statistik BPS Kabupaten Kepulauan Sula 2024")
+def main():
 
-st.header('Rekapan')
+    st.title("Monitoring Rekrutmen Mitra Statistik BPS Kabupaten Kepulauan Sula 2024")
 
-pendaftar_sula = data[data['wilayah']=='Kepulauan Sula']['posisi_daftar'].value_counts()
-pendaftar_taliabu = data[data['wilayah']=='Pulau Taliabu']['posisi_daftar'].value_counts()
+    st.header('Rekapan')
 
-tab1, tab2 = st.tabs(["Kepulauan Sula", "Pulau Taliabu"])
-with tab1:
-    st.subheader(f"Tanggal Update: {data[data['wilayah']=='Kepulauan Sula']['date_update'].unique()[0]} WIT")
-    st.subheader(f"Total pendaftar: {pendaftar_sula.sum()}")
-    st.dataframe(pendaftar_sula)
-with tab2:
-    st.subheader(f"Tanggal Update: {data[data['wilayah']=='Pulau Taliabu']['date_update'].unique()[0]} WIT")
-    st.subheader(f"Total pendaftar: {pendaftar_taliabu.sum()}")
-    st.dataframe(pendaftar_taliabu)
+    pendaftar_sula = data[data['wilayah']=='Kepulauan Sula']['posisi_daftar'].value_counts()
+    pendaftar_taliabu = data[data['wilayah']=='Pulau Taliabu']['posisi_daftar'].value_counts()
 
-st.markdown("---")
+    tab1, tab2 = st.tabs(["Kepulauan Sula", "Pulau Taliabu"])
+    with tab1:
+        st.subheader(f"Tanggal Update: {data[data['wilayah']=='Kepulauan Sula']['date_update'].unique()[0]} WIT")
+        st.subheader(f"Total pendaftar: {pendaftar_sula.sum()}")
+        st.dataframe(pendaftar_sula)
+    with tab2:
+        st.subheader(f"Tanggal Update: {data[data['wilayah']=='Pulau Taliabu']['date_update'].unique()[0]} WIT")
+        st.subheader(f"Total pendaftar: {pendaftar_taliabu.sum()}")
+        st.dataframe(pendaftar_taliabu)
 
-st.subheader('Daftar nama calon mitra yang berhasil mendaftar:')
-with st.container():
-    col1, col2 = st.columns(2)
-    with col1:
-        posisi_daftar = st.multiselect(
-            "Pilih posisi daftar:",
-            options=data["posisi_daftar"].unique(),
-            default=data["posisi_daftar"].unique()
+    st.markdown("---")
+
+    st.subheader('Daftar nama calon mitra yang berhasil mendaftar:')
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            posisi_daftar = st.multiselect(
+                "Pilih posisi daftar:",
+                options=data["posisi_daftar"].unique(),
+                default=data["posisi_daftar"].unique()
+            )
+        with col2:
+            wilayah = st.multiselect(
+                "Pilih wilayah pendaftaran:",
+                options=data["wilayah"].unique(),
+                default=data["wilayah"].unique()
+            )
+
+        data_filtered = data.query(
+            "posisi_daftar == @posisi_daftar & wilayah == @wilayah"
         )
-    with col2:
-        wilayah = st.multiselect(
-            "Pilih wilayah pendaftaran:",
-            options=data["wilayah"].unique(),
-            default=data["wilayah"].unique()
-        )
 
-    data_filtered = data.query(
-        "posisi_daftar == @posisi_daftar & wilayah == @wilayah"
-    )
+        data_filtered = data_filtered.loc[:, data_filtered.columns != "date_update"]
 
-    data_filtered = data_filtered.loc[:, data_filtered.columns != "date_update"]
+        st.dataframe(data_filtered, use_container_width=True)
 
-    st.dataframe(data_filtered, use_container_width=True)
+        # Rerun aplikasi dan membersihkan cache saat halaman diperbarui
+        st.experimental_rerun()
+
+if __name__ == "__main__":
+    main()
