f0009117393307e5288554f610536fd587c819d7
nabilabptr <75235781+nabilabptr@users.noreply.github.com>
2023-11-14 12:23:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.set_page_config(layout="wide")
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-data = conn.read(worksheet="db", usecols=list(range(3)))
+data = conn.read(worksheet="db", usecols=list(range(4)))
 data = data.dropna(how="all")
 
 st.title("Monitoring Rekrutmen Mitra Statistik BPS Kabupaten Kepulauan Sula 2024")
@@ -17,9 +17,11 @@ pendaftar_taliabu = data[data['wilayah']=='Pulau Taliabu']['posisi_daftar'].valu
 
 tab1, tab2 = st.tabs(["Kepulauan Sula", "Pulau Taliabu"])
 with tab1:
+    st.subheader(f"Tanggal Update: {data[data['wilayah']=='Kepulauan Sula']['date_update'].unique()[0]} WIT")
     st.subheader(f"Total pendaftar: {pendaftar_sula.sum()}")
     st.dataframe(pendaftar_sula)
 with tab2:
+    st.subheader(f"Tanggal Update: {data[data['wilayah']=='Pulau Taliabu']['date_update'].unique()[0]} WIT")
     st.subheader(f"Total pendaftar: {pendaftar_taliabu.sum()}")
     st.dataframe(pendaftar_taliabu)
 
@@ -45,13 +47,7 @@ with st.container():
         "posisi_daftar == @posisi_daftar & wilayah == @wilayah"
     )
 
+    data_filtered = data_filtered.loc[:, data_filtered.columns != "date_update"]
+
     st.dataframe(data_filtered, use_container_width=True)
-
-
-
-
-
-
-
-
 
